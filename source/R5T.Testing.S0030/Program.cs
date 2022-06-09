520f738bc22f8ae336ddcade3eea631d817a30b6
davidcoats <david@getsafetycone.com>
2022-06-09 12:41:50 -0700

    Interim changes.

---
+++ source/R5T.Testing.S0030/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Testing.S0030
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
